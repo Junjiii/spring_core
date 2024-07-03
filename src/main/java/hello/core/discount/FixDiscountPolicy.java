1d7671ca07ec18a88b3609df526bf5f711ed4e5d
Jjunjiii’ <drum1701@gmail.com>
2024-07-03 10:04:59 +0900

    discount policy

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixPolicy = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixPolicy;
+        } else {
+            return 0;
+        }
+    }
+
+}
